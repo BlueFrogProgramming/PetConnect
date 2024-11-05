81306288f35e94b820e82542fad0de5b26ae24f5
BlueFrogProgramming <thebluefroggamer@gmail.com>
2024-11-05 20:29:45 +0000

    remove sensitive info from s3.js

--- server/s3.js
+++ server/s3.js
@@ -2,8 +2,8 @@ import aws from 'aws-sdk'
 
 const region = "eu-west-2"
 const bucketName = "amplify-petconnect-dev-d1727-deployment"
-const accessKeyId = "AKIA2HVQ47W3CSN4QODK"
-const secretAccessKey = "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"
+const accessKeyId = ""
+const secretAccessKey = ""
 
 const s3 = new aws.S3({
     region,
